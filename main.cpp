4299e1287d6f7a91d81b96640fc96ea8ef530e85
YoavNahumus <yoavnahum2005@gmail.com>
2024-06-27 18:52:55 +0300

    added a test to main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
-#include "graph.hpp"
+#include "ADO.hpp"
 #include <iostream>
 
 int main(int argc, char const *argv[]) {
-    
+    Graph* graph = new Graph(10);
+    graph->addEdge(0, 1, 1);
+    graph->addEdge(0, 2, 2);
+    graph->addEdge(0, 3, 3);
+    graph->addEdge(1, 2, 4);
+    graph->addEdge(1, 3, 5);
+    graph->addEdge(2, 3, 6);
+    graph->addEdge(2, 4, 7);
+    graph->addEdge(3, 4, 8);
+    graph->addEdge(3, 5, 9);
+    graph->addEdge(4, 5, 10);
+    graph->addEdge(5, 6, 1);
+    graph->addEdge(5, 7, 2);
+    graph->addEdge(5, 8, 3);
+    graph->addEdge(6, 7, 4);
+    graph->addEdge(6, 8, 5);
+    graph->addEdge(7, 8, 6);
+    graph->addEdge(7, 9, 7);
+    graph->addEdge(8, 9, 8);
+    ADO* ado = new ADO(graph, 3);
+    ado->preprocess(true);
+
+    for (int i = 0; i < graph->vertexCount; i++) {
+        for (auto &&p : *ado->bunches[i]) {
+            std::cout << "distance from " << i << " to " << p.first << ": " << p.second << std::endl;
+        }
+    }
+
+    delete ado;
+    delete graph;
+
     return 0;
 }
